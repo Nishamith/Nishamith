a25c0d311cb5370f92ae75f458cc9f84de6c75fc
Nishamith <nishamith05@gmail.com>
2022-01-20 14:51:49 +0530

    Amith

--- src/org/air/Air.java
+++ src/org/air/Air.java
@@ -1,12 +1,22 @@
 package org.air;
 
 public class Air {
-	
- public void aeroplane() {
-  System.out.println("Aeroplane CompanyNames:Spicejet,JetAirways");
-}
- public void helicopter() {
-  System.out.println("Helicopter other Name: Chopper");
+	private void debts() {
+		System.out.println("debts");
 	}
-
+	
+	
+	private void income() {
+		System.out.println("income");
+	}
+	
+	public static void main(String[] args) {
+		Air a=new Air();
+		a.debts();
+		a.income();
+		
+		
+	}
 }
+	
+ 
